40b7f23da22c585daba1e2d50862a083296868fa
Steffonell <stefandakic1231@gmail.com>
2023-04-14 17:01:24 +0200

    Added fix for context being lost when refresh page

--- src/components/ApplicationContext.js
+++ src/components/ApplicationContext.js
@@ -3,10 +3,57 @@ import { createContext, useEffect, useState } from "react";
 export const ApplicationContext = createContext(null);
 
 export const ApplicationContextProvider = (props) => {
-  const [loggedInClient, setLoggedInClient] = useState({});
-  const [basketItems, setBasketItems] = useState([]);
-  const [token, setToken] = useState(null);
-  const [authenticated, setAuthenticated] = useState(false);
+  // Load state from local storage
+  const loadStateFromLocalStorage = () => {
+    try {
+      const serializedState = localStorage.getItem("myAppState");
+      if (serializedState === null) {
+        return {
+          loggedInClient: {},
+          basketItems: [],
+          token: null,
+          authenticated: false,
+        };
+      }
+      return JSON.parse(serializedState);
+    } catch (error) {
+      console.error("Error loading state from local storage:", error);
+      return {
+        loggedInClient: {},
+        basketItems: [],
+        token: null,
+        authenticated: false,
+      };
+    }
+  };
+
+  const initialState = loadStateFromLocalStorage();
+
+  const [loggedInClient, setLoggedInClient] = useState(initialState.loggedInClient);
+  const [basketItems, setBasketItems] = useState(initialState.basketItems);
+  const [token, setToken] = useState(initialState.token);
+  const [authenticated, setAuthenticated] = useState(initialState.authenticated);
+
+  // Save state to local storage when state changes
+  useEffect(() => {
+    const state = {
+      loggedInClient,
+      basketItems,
+      token,
+      authenticated,
+    };
+    saveStateToLocalStorage(state);
+  }, [loggedInClient, basketItems, token, authenticated]);
+
+  // Saving state to local storage
+  const saveStateToLocalStorage = (state) => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem("myAppState", serializedState);
+    } catch (error) {
+      console.error("Error saving state to local storage:", error);
+    }
+  };
 
   const addOrUpdateBasketItem = (article, quantity) => {
     const existingItem = basketItems.find(
