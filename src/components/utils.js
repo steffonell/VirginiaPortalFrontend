1491146245b4a739e829da384a8adbd9df361449
Steffonell <stefandakic1231@gmail.com>
2023-10-19 22:31:53 +0200

    Build VIR-38, VIR-39

--- src/components/utils.js
+++ src/components/utils.js
@@ -15,3 +15,15 @@ export function getAllowedRoles() {
   return ["ROLE_ADMIN", "ROLE_USER", "ROLE_MODERATOR", "ROLE_FAKTURISTA", "ROLE_MAGACIONER"];
 }
 
+
+const articlePriceWithDiscount = (article) => {
+  return (Number(article.retailPrice) * (1 - Number(brandDiscount(article.brand)) / 100)).toFixed(2);
+}
+
+const discountedPrice = (price, discount) => {
+  return (Number(price) * (1 - Number(discount / 100)).toFixed(2));
+}
+
+const priceWithPDV = (price, discount) => {
+  return (Number(price) * (1 + Number(discount / 100)).toFixed(2));
+}
